8fdeadc9041415abeab2d566bc172729d36049d5
Franco <ferrufinofranco@hotmail.com>
2023-03-27 02:14:59 -0300

    Second Commit

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,7 +3,7 @@ from AppCoder.views import *
 
 urlpatterns = [
     path('home/', home, name="Home"),
-    path('cursos/', registroCursos, name="AppCoderCursos"),
+    path('registroCursos/', registroCursos, name="AppCoderCursos"),
     path('busquedaCursos/', busquedaCursos, name="AppCoderBusquedaCursos"),
     path('registroProfesores/', registroProfesores, name='AppCoderRegistroProfesores'),
     path('registroEstudiante/', registroEstudiante, name='AppCoderRegistroEstudiante')
